7dde08569fee2cb4279d9d0b930d47f081b64b10
Josy Santana <jjsantanafronteli@hotmail.com>
2022-12-12 09:27:46 -0300

    exercícios do curso

---
+++ aula12/exerc1.js
@@ -0,0 +1,10 @@
+var idade = 32;
+if (idade < 18){
+    console.log('Menor de Idade');
+} else {
+    if(idade >60){
+        console.log('Maior de idade');
+    } else {
+        console.log('Pague suas contas.');
+    }
+ }
