11881d2053afa426a0970c537bd95319daee008e
Josy Santana <jjsantanafronteli@hotmail.com>
2022-12-20 13:40:02 -0300

    exercicio extra

---
+++ moduloF/script.js
@@ -0,0 +1,21 @@
+function calculo(){
+    var gas = document.getElementById('g')
+    var etanol = document.getElementById('e')
+    
+    if (gas.value.length == 0 || etanol.value.length == 0){
+        window.alert('Preencha todos os campos corretamente.')
+    }else{
+        res.innerHTML = ''
+        var g = Number(gas.value)
+        var e = Number(etanol.value)
+        
+        let result = e / g
+        
+        if (result > 0.7 ) {
+            res.innerHTML = "Abasteça com etanol" 
+        }
+        else {
+            res.innerHTML = "Abasteça com gasolina"
+        }
+    }
+}      
