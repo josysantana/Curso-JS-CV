52e7e015fbf3d16fe0a3cabb98df6cfe1cdb0884
Josy Santana <jjsantanafronteli@hotmail.com>
2022-12-21 13:17:34 -0300

    alteração das pastas de exercícios

--- ModuloD/aula 14/script.js
+++ ModuloD/aula 14/script.js
@@ -6,9 +6,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora <= 12){
         img.src = 'image/manha.jpg'
+        document.body.style.background = '#e2cdf9'
     } else if(hora >= 13 && hora <= 18){
         img.src = 'image/tarde.jpg'
+        document.body.style.background = '#b9846f'
     } else{
         img.src = 'image/noite.jpg'
+        document.body.style.background = '#515154'
     }
 }
