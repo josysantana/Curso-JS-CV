12687a8aab79072c00efeb23e9d479106bedada6
Josy Santana <jjsantanafronteli@hotmail.com>
2023-01-04 11:48:41 -0300

    atualização do exerc 18

---
+++ ModuloF/exerc8/img/array.js
@@ -0,0 +1,6 @@
+vetor = [3,9,4,8,1,7]
+ordem = vetor.sort()
+ultimo = ordem[ordem.length -1]
+console.log(ordem)
+console.log(typeof(ordem))
+console.log(ultimo)
--- moduloF/script18.js
+++ moduloF/script18.js
@@ -20,9 +20,39 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumero(numero.value) && !inLista(numero.value, vetor)){
-        vetor = numero
-        
+        vetor.push(Number(numero.value))
+        let item = document.createElement('option')
+        item.text = `O valor ${numero.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else{
-        window.alert('nada')
+        window.alert('Número inválido ou já se encontra na lista.')
     }
+    num.value = ''
+    num.focus
+}
+
+function finish(){
+    if(vetor.length == 0){
+        window.alert('Nenhum número para finalizar.')
+    } else {
+        let total = vetor.length
+        let ordem = vetor.sort()
+        let ultimo = ordem[ordem.length -1]
+        let primeiro = ordem[0]
+        let soma = 0
+        let media = 0
+        for(let pos in vetor){
+            soma += vetor[pos]
+        }
+        media = soma / total
+        res.innerHTML = ""
+        res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p> O maior número informado é ${ultimo}</p>`
+        res.innerHTML += `<p> O menor número informado é ${primeiro}</p>`
+        res.innerHTML += `<p> A soma dos números informados é ${soma}</p>`
+        res.innerHTML += `<p> A média dos números informados é ${media}</p>`
+    }
+
+
 }
